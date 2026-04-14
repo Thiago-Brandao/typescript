3248709323c9b6fcd1f002722fc2fd124fd84973
Thiago Brandao <thiagobmo2006@gmail.com>
2026-04-14 15:11:10 -0300

    Projeto configurado com gitignore e arquivos base, usando typescript

---
+++ src/aula-03.ts
@@ -0,0 +1,13 @@
+interface Pessoa {
+    idade: number;
+    nome: string;
+    ismaioridade: boolean;
+};
+
+let pessoa: Pessoa = {
+    idade: 543,
+    nome: 'Algusto',
+    ismaioridade: true
+};
+
+console.log(pessoa);
